3647579c20bbbea13522dd24be8b0d0a5db6060b
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-05-21 01:29:00 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux'
+
+import { Provider } from 'react-redux'
+
+import rootReducer from './redux/reducers'
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/theme.css'
+import './assets/css/indexs.css'
+
+
+import Layout from './component/Layout/Layout'
+
+const store = createStore(
+  rootReducer
+)
+
+
+document.title = 'CI finesser'
+ReactDOM.render(
+  <Provider store={store}>
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
